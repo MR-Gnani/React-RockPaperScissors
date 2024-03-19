eb069a206a3ff19f38c2b369ba4bbaf8790bf33d
nani kim <flsk9698@naver.com>
2024-03-19 14:19:01 +0900

    프로젝트 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// 1. 박스 2개 (title, image, result)
+// 2. 가위바위보 버튼
+// 3. 버튼 클릭시 클릭한 값이 박스에 랜더링
+// 4. 컴퓨터는 랜덤
+// 5. 3과4의 결과에 따라 승패가 정해짐
+// 6. 이기면 초록, 지면 빨강, 비기면 검정
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
     </div>
   );
 }
